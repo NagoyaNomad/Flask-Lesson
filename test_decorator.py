20d7bc3b3cb916f64b2ee05cb7be8df01543ba9f
Kamio Hiroshi <nagoya.nomad@gmail.com>
2025-08-12 03:57:06 +0900

    デコレータの機能について確認する。
    （スタートとなる内容に戻した。）
    元となる函数定義。函数がいくつもあり、そこで同じ処理を行っている部分がある。
    重複しているものは、まとめて一元管理したい！

--- test_decorator.py
+++ test_decorator.py
@@ -1,25 +1,22 @@
 ### Pythonのデコレータを理解するためのコード
 
-## 函数内の同じ処理を行なっている部分をくくり出す。
-## ---> 函数を引数とする函数を導入する！
+## デコレータ適用前の函数とその実行
 
-# メインの処理をする函数
 def operation():
+    print('前処理を行います。')
+
     print('メインの処理を行います。')
 
+    print('後処理を行います。')
+
 def operation_2():
+    print('前処理を行います。')
+
     print('２つ目の処理を行います。')
 
-
-# 函数を引数に取る函数を定義して、同じ処理をくくり出す！
-def outer_process(func):
-    print('==前処理を行います。==')
-
-    func()
-
-    print('==後処理を行います。==')
+    print('後処理を行います。')
 
 
 if __name__ == "__main__":
-    outer_process(operation)
-    outer_process(operation_2)
+    operation()
+    operation_2()
