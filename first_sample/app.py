9312202b2a60a89ce377b4d67449df3d8d1037aa
NagoyaNomad <nomad.nagoya@gmail.com>
2025-08-12 03:37:39 +0900

    Add files via upload

---
+++ first_sample/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+# インスタンス生成
+app = Flask(__name__)
+
+# ルーティング
+@app.route('/')
+def hello_world():
+    return '<h1>Hello World!</h1>'
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
