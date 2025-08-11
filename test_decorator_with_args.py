87e7769ff7a853b6ee981b6ee41395cdf99c1376
Kamio Hiroshi <nagoya.nomad@gmail.com>
2025-08-12 05:25:58 +0900

    オライリー本のサンプル
    デコレータ内部で処理する函数の引数を使う。

--- test_decorator_with_args.py
+++ test_decorator_with_args.py
@@ -17,13 +17,25 @@ def outer(func):
     # 入力の函数（引数func）に処理を追加した別の函数を返す！
     return inner
 
+# 別のデコレータ（オライリー本の例）
+def document_it(func):
+    def new_function(*args, **kwargs):
+        print('Running function:', func.__name__)
+        print('Positional arguments:', args)
+        print('Keyword arguments:', kwargs)
+        print('==渡した函数の処理==')
+        result = func(*args, **kwargs)
+        print('Result:', result)
+        return result
+    return new_function
+
 
 # 処理をするデータ
 nums = (10, 20, 30, 40)
 user_dict = {'やまさん': 30, 'てっちゃん': 40, 'くまさん': 50}
 
 # メインの処理をする函数（デコレータをつける）
-@outer
+@document_it
 def show_sum(nums):
     summary = 0
     for num in nums:
@@ -31,7 +43,7 @@ def show_sum(nums):
 
     print(f'{nums}の合計：{summary}')
 
-@outer
+@document_it
 def show_info(users):
     for name, age in users.items():
         print(f'名前：{name}、年齢：{age}')
