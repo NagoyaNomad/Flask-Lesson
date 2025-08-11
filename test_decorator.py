0340f7da54e3e62cb50caf0c59fc8efdafad96d9
Kamio Hiroshi <nagoya.nomad@gmail.com>
2025-08-12 03:59:44 +0900

    函数を引数にとる函数を導入した。
    Pythonでは函数もオブジェクトなので、変数や引数として扱える。
    この利点を使って、メインの処理を行なっている函数から重複している処理をくくり出すことができる！
    ただし、実際に実行するのは新規に用意した「函数を引数にとる函数」となる。<-- 改善したい！

--- test_decorator.py
+++ test_decorator.py
@@ -1,22 +1,25 @@
 ### Pythonのデコレータを理解するためのコード
 
-## デコレータ適用前の函数とその実行
+## 函数内の同じ処理を行なっている部分をくくり出す。
+## ---> 函数を引数とする函数を導入する！
 
+# メインの処理をする函数
 def operation():
-    print('前処理を行います。')
-
     print('メインの処理を行います。')
 
-    print('後処理を行います。')
-
 def operation_2():
-    print('前処理を行います。')
-
     print('２つ目の処理を行います。')
 
-    print('後処理を行います。')
+
+# 函数を引数に取る函数を定義して、同じ処理をくくり出す！
+def outer_process(func):
+    print('==前処理を行います。==')
+
+    func()
+
+    print('==後処理を行います。==')
 
 
 if __name__ == "__main__":
-    operation()
-    operation_2()
+    outer_process(operation)
+    outer_process(operation_2)
