c53809c450aa2676f1558d8d192b350aaa1c5857
Kamio Hiroshi <nagoya.nomad@gmail.com>
2025-08-12 04:21:45 +0900

    函数内函数を導入した。
    この段階では、実行時には手動でデコレータ処理を実施する。

--- test_decorator.py
+++ test_decorator.py
@@ -1,7 +1,7 @@
 ### Pythonのデコレータを理解するためのコード
 
 ## 函数内の同じ処理を行なっている部分をくくり出す。
-## ---> 函数を引数とする函数を導入する！
+## ---> 函数内函数を導入する！！
 
 # メインの処理をする函数
 def operation():
@@ -12,14 +12,26 @@ def operation_2():
 
 
 # 函数を引数に取る函数を定義して、同じ処理をくくり出す！
-def outer_process(func):
-    print('==前処理を行います。==')
+def outer(func):
+    # 函数内函数の定義
+    def inner():
+        print('==前処理を行います。==')
 
-    func()
+        # 実際の処理（これも函数内函数であることに注意！）
+        func()
 
-    print('==後処理を行います。==')
+        print('==後処理を行います。==')
+
+    # 入力の函数（引数func）に処理を追加した別の函数を返す！
+    return inner
 
 
 if __name__ == "__main__":
-    outer_process(operation)
-    outer_process(operation_2)
+    # デコレータ処理を手動で行う。
+    # Flask本ではなぜこのリストがあるのかわからなかったが、
+    # 「入門Python3、O'Rilley」のデコレータの説明を読んで判った！
+    result = outer(operation)
+    result()
+
+    result_2 = outer(operation_2)
+    result_2()
